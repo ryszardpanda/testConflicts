48ae544987018824f00705261e52a043c417e265
Marcin Sobolewski <mlodysobol@gmail.com>
2024-05-25 17:19:00 +0200

    feature/alejazz2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Conflictssss synuś");
+        System.out.println("Conflictssssssss synuś");
     }
 }
