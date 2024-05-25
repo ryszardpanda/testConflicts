901b4fc49c8e3f24341c75dacffd6c4713239c33
Ryszard Różański <rychu48@op.pl>
2024-05-25 16:40:57 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Conflictssss synuś");
+        System.out.println("Conflictssss sysxdascxasxascxsacxwscxwscxcxsqcxqscxswcxsw   cxnuś");
     }
 }
